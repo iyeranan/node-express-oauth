31e3f7ba697171b8de65e6cdfd8e84b0b5c18e13
Andy <iyeranan@gmail.com>
2020-10-23 21:18:07 +1100

    Fixing all bugs

--- authorization-server.js
+++ authorization-server.js
@@ -99,10 +99,12 @@ app.post('/token', function (req, res) {
 		res.status(401).end();
 	} else {
 		const client = decodeAuthCredentials(req.headers.authorization);
-		if (!clients[client.clientId] || (clients[client.clientId] !== client.clientSecret) || (!authorizationCodes[req.body.code])) {
+
+		if (!clients[client.clientId] || (clients[client.clientId].clientSecret !== client.clientSecret) || !authorizationCodes[req.body.code]) {
 			res.status(401).end();
 		} else {
 			const obj = authorizationCodes[req.body.code];
+			delete authorizationCodes[req.body.code];
 			const token = jwt.sign({ userName: obj.userName, scope: obj.clientReq.scope }, config.privateKey, { algorithm: 'RS256' });
 			res.status(200).send({ "access_token": token, "token_type": 'Bearer' });
 		}
